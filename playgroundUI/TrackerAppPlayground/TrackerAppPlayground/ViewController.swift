cafaebcc7a843468a2b8da1213f967ff071d22aa
Daphne <daphne.larose@gmail.com>
2020-04-12 00:51:47 -0700

    Added folders
    added playgroundUI for playing around with UI.
    
    added dataStorage for playing around with data models, data storage (e.g. core data, sqlite) and swift.

---
+++ playgroundUI/TrackerAppPlayground/TrackerAppPlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrackerAppPlayground
+//
+//  Created by Daphne Larose on 4/11/20.
+//  Copyright © 2020 Team Jackie-Daph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
